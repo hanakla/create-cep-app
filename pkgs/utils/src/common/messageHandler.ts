d90a66e74569b6c005e322300bf13414a90a5d48
Hanakla <8597982+hanakla@users.noreply.github.com>
2025-06-08 02:12:13 +0900

    wip: ci

--- pkgs/utils/src/common/messageHandler.ts
+++ pkgs/utils/src/common/messageHandler.ts
@@ -1,3 +1,22 @@
+/**
+ * Create ExtendScript env message handler.
+ * This handler receives messages from CEP(frontend) environment
+ * that use with `postMessageToHost()` function.
+ *
+ * @example
+ * ```ts
+ * const handler = createMessageHandler()
+ *   .event("test", (a: number, b: string) => {
+ *     return `Received: ${a}, ${b}`;
+ *   })
+ *
+ * // Register handler to global object
+ * $.global.messageHandler = messageHandler.handler;
+ *
+ * // In CEP environment, you can send message like this:
+ * postMessageToHost("test", [42, "Hello"]);
+ * ```
+ */
 export const createMessageHandler = () => {
   return new MessageHandler();
 };
@@ -7,6 +26,11 @@ class MessageHandler<
 > {
   private handlers: any = {};
 
+  /**
+   * Register a message handler
+   * @param event - Event name you define
+   * @param handler - Handler function that receives payload and returns response
+   */
   public event<E extends string, P extends Array<any>, R>(
     event: E,
     handler: (...args: P) => R
@@ -17,6 +41,7 @@ class MessageHandler<
     return this as any;
   }
 
+  /** Execute specified handler */
   public handler = <E extends keyof T>({
     type,
     payload,
@@ -24,15 +49,53 @@ class MessageHandler<
     type: E;
     payload: T[E]["payload"];
   }): string => {
-    return JSON.stringify(this.handlers[type](...payload) ?? null);
+    return JSON.stringify(this.handlers[type].apply(null, payload) ?? null);
   };
 }
 
+/**
+ * Get name and payload types of registered message by handler
+ *
+ * @example
+ * ```ts
+ * const handler = createMessageHandler()
+ *   .event("test", (a: number, b: string) => {
+ *     return `Received: ${a}, ${b}`;
+ *   })
+ *   .event("test2", (a: number) => {
+ *     return `Received: ${a}`;
+ *   });
+ *
+ * type RequestTypes = MessageRequestTypesOf<typeof handler>;
+ * // RequestTypes will be:
+ * // {
+ * //   test: [number, string];
+ * //   test2: [number];
+ * // }
+ * ```
+ */
 export type MessageRequestTypesOf<T extends MessageHandler<any>> =
   T extends MessageHandler<infer T>
     ? { [K in keyof T]: T[K]["payload"] }
     : never;
 
+/**
+ * Get response types of registered message by handler
+ *
+ * @example
+ * ```ts
+ * const handler = createMessageHandler()
+ *   .event("test", (a: number, b: string) => {
+ *     return `Received: ${a}, ${b}`;
+ *   })
+ *
+ * type ResponseTypes = MessageResponseTypeOf<typeof handler>;
+ * // ResponseTypes will be:
+ * // {
+ * //   test: string;
+ * // }
+ * ```
+ */
 export type MessageResponseTypeOf<T extends MessageHandler<any>> =
   T extends MessageHandler<infer T>
     ? { [K in keyof T]: T[K]["response"] }
